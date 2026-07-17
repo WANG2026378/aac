87a17d08fb228b6fd40a206e60b4a02f2c8a2baf
charm <charm@charmdeMacBook-Pro.local>
2026-07-17 16:21:40 +0800

    Add Sanguo math strategy game

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* iPad offline pack for the teaching portal. */
 importScripts('offline-files.js');
 
-var VERSION = '20260717-sanguo-tactic-v1';
+var VERSION = '20260717-sanguo-math-v1';
 var CACHE_PREFIX = 'aac-teaching-';
 var STATIC_CACHE = CACHE_PREFIX + 'static-' + VERSION;
 var RUNTIME_CACHE = CACHE_PREFIX + 'runtime-' + VERSION;
