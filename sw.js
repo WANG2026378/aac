70b9462079d5d458a003b746f4ea750a485acaf1
charm <charm@charmdeMacBook-Pro.local>
2026-08-02 21:14:56 +0800

    Publish Zouzu Zhuan v7 bosses and music

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* iPad offline pack for the teaching portal. */
 importScripts('offline-files.js');
 
-var VERSION = '20260802-zouzu-v6';
+var VERSION = '20260802-zouzu-v7';
 var CACHE_PREFIX = 'aac-teaching-';
 var STATIC_CACHE = CACHE_PREFIX + 'static-' + VERSION;
 var RUNTIME_CACHE = CACHE_PREFIX + 'runtime-' + VERSION;
