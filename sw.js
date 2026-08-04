94b098be4f5673d47687b5d7e68855f8a70bd516
WANG2026378 <charmming.tw@yahoo.com.tw>
2026-08-04 23:31:35 +0800

    auto-deploy（急件） 2026-08-04 23:31

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* iPad offline pack for the teaching portal. */
 importScripts('offline-files.js');
 
-var VERSION = '20260804-food-spirit';
+var VERSION = '20260804-mole-dot-v1';
 var CACHE_PREFIX = 'aac-teaching-';
 var STATIC_CACHE = CACHE_PREFIX + 'static-' + VERSION;
 var RUNTIME_CACHE = CACHE_PREFIX + 'runtime-' + VERSION;
